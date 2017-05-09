7876f41e986840e340a07b2c935ca5454b6da1c0
Jeff848 <jeffreyma.wii@gmail.com>
2017-05-09 08:34:48 -0700

    testing...

---
+++ src/Plane.java
@@ -0,0 +1,9 @@
+
+public class Plane extends MovingImage{
+
+	public Plane(String filename, int x, int y, int w, int h) {
+		super(filename, x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Projectile.java
@@ -0,0 +1,9 @@
+
+public class Projectile extends MovingImage{
+
+	public Projectile(String filename, int x, int y, int w, int h) {
+		super(filename, x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+}
